93600bb575cb2ffa3a295cfc6972ff5991680be3
Justin Caratzas <bigjust@lambdaphil.es>
2016-10-30 19:02:01 -0400

    remove test

--- tests/test_default.py
+++ tests/test_default.py
@@ -9,10 +9,3 @@ def test_postgresql_service(Service):
 
     assert postgresql_service.is_running
     assert postgresql_service.is_enabled
-
-
-def test_postgresql_database(Command):
-
-    cmd = Command("psql")
-
-    assert cmd.rc == 0
