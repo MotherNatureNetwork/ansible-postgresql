a6ddced456c9ad83486f5b350714cf0e7011f787
Justin Caratzas <bigjust@lambdaphil.es>
2016-10-30 10:25:55 -0400

    create user/database by default

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,3 +4,10 @@ def test_postgresql_service(Service):
 
     assert postgresql_service.is_running
     assert postgresql_service.is_enabled
+
+
+def test_postgresql_database(Command):
+
+    cmd = Command("psql")
+
+    assert cmd.rc == 0
