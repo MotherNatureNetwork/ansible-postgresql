21a07181adf782ed2f7b71910d842cd16956c6e4
Justin Caratzas <bigjust@lambdaphil.es>
2016-10-30 16:48:29 -0400

    try this for travis

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,5 +1,5 @@
 from testinfra.utils.ansible_runner import AnsibleRunner
-testinfra_hosts = AnsibleRunner('../.molecule/ansible_inventory')\
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory')\
                   .get_hosts('all')
 
 
