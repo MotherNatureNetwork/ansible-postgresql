ebf28f4211794a7b7f1e3e8b2dbbde32e2d21d3b
Justin Caratzas <bigjust@lambdaphil.es>
2018-07-21 01:56:14 -0400

    Service module is deprecated, switch to host.service

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,8 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_postgresql_service(Service):
-
-    postgresql_service = Service('postgresql')
-
-    assert postgresql_service.is_running
+def test_postgresql_running_and_enabled(host):
+    psql = host.service("postgresql")
+    assert psql.is_running
+    assert psql.is_enabled
